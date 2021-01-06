0aa3a5eedce492af516a1fbe95a4ea140c73e971
itsyaboikris <kristoff_riley@outlook.com>
2021-01-06 11:41:48 -0400

    Initial Commit (Sign In, Sign Up, Forgot Password, Update Profile)

---
+++ src/components/Dashboard.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import {Link, useHistory} from "react-router-dom";
+import {Alert, Button, Card} from "react-bootstrap";
+import {useAuth} from "../context/AuthContext";
+
+export default function Dashboard() {
+
+    const [error, setError] = useState("");
+    const {currentUser, logout} = useAuth();
+    const history = useHistory();
+
+    async function handleLogout() {
+        setError("");
+        
+        try{
+            await logout();
+            history.push("/login")
+        }catch (e) {
+            setError("Failed to logout");
+        }
+    }
+    
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>{currentUser.email}</strong>
+                    <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                <Button variant="link" onClick={handleLogout}>Log Out</Button>
+            </div>
+        </>
+    )
+}
---
+++ src/components/UpdateProfile.js
@@ -0,0 +1,70 @@
+import {Form, Button, Card, Alert} from "react-bootstrap";
+import React, {useRef, useState} from "react";
+import {useAuth} from "../context/AuthContext";
+import {Link, useHistory} from "react-router-dom";
+
+function UpdateProfile() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+    const {updatePassword, updateEmail, currentUser} = useAuth();
+    const history = useHistory();
+
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if (passwordRef.current.value !== confirmPasswordRef.current.value){
+            return setError("Passwords do not match");
+        }
+
+        setLoading(true);
+
+        const promises =[];
+        if(emailRef.current.value !== currentUser.email){
+            promises.push(updateEmail(emailRef.current.value));
+        }
+        if(passwordRef.current.value){
+            promises.push(updatePassword(passwordRef.current.value));
+        }
+
+        Promise.all(promises).then( () =>{
+            history.push("/");
+        }).catch( () => {
+            setError("Failed to update account")
+        }).finally( () => {
+            setLoading(false);
+        })
+    }
+
+    return (
+        <>
+            <Card>
+                <h2 className="text-center mb-4">Update Profile</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" ref={emailRef} required defaultValue={currentUser.email}/>
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} placeholder="Leave blank to keep same password"/>
+                    </Form.Group>
+                    <Form.Group id="confirmPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" ref={confirmPasswordRef} placeholder="Leave blank to keep same password"/>
+                    </Form.Group>
+                    <Button disabled={loading} type="submit" className="w-100">Update</Button>
+                </Form>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                <Link to="/">Cancel</Link>
+            </div>
+        </>
+    );
+}
+
+export default UpdateProfile;
